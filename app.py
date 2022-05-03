35322dc3749dde71a59d7e29963283d5f1745552
gaurav.j <gaurav.j@media.net>
2022-05-03 19:18:41 +0530

    added new page teammatchups and refactored filtering methods in utils page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import  home,batsmananalysis,bowleranalysis,teamanalysis,venueanalysis,battercomparison,bowlercomparison,battermatchups,bowlermatchups,predictwinpercent,scorepredictor # import your app modules here
+from apps import  home,batsmananalysis,bowleranalysis,teamanalysis,venueanalysis,battercomparison,bowlercomparison,battermatchups,bowlermatchups,teammatchups,predictwinpercent,scorepredictor # import your app modules here
 
 app = MultiApp()
 
@@ -8,12 +8,13 @@ app = MultiApp()
 app.add_app("Home", home.app)
 app.add_app("Batting Records", batsmananalysis.app)
 app.add_app("Bowling Records", bowleranalysis.app)
-app.add_app("Team Analysis", teamanalysis.app)
+app.add_app("Team Records", teamanalysis.app)
 app.add_app("Venue Analysis", venueanalysis.app)
 app.add_app("Batter Comparison", battercomparison.app)
 app.add_app("Bowler Comparison", bowlercomparison.app)
 app.add_app("Batter Matchups", battermatchups.app)
 app.add_app("Bowler Matchups", bowlermatchups.app)
+app.add_app("Team Matchups", teammatchups.app)
 app.add_app("Win Predictor", predictwinpercent.app)
 app.add_app("Score Predictor", scorepredictor.app)
 
