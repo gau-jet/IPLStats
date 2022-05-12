372a8acdf33a586462009273937c8e4a935091d9
gaurav.j <gaurav.j@media.net>
2022-05-12 13:46:39 +0530

    using new matches and deliveries file and added season as filter

--- apps/venueanalysis.py
+++ apps/venueanalysis.py
@@ -10,8 +10,8 @@ def app():
     utils.header(st)
     st.title('Venue Records')    
     
-    del_df = utils.return_df("data/IPL Ball-by-Ball 2008-2022.csv")
-    match_df = utils.return_df("data/IPL Matches 2008-2022.csv")
+    del_df = utils.return_df("data/deliveries.csv")
+    match_df = utils.return_df("data/matches.csv")
     player_df = utils.return_df("data/Player Profile.csv")
     comb_df = pd.merge(del_df, match_df, on = 'id', how='left')
           
@@ -21,37 +21,43 @@ def app():
     #comb_df = comb_df.replace(np.NaN, 0)
     #st.write(comb_df.head(10))
     
+    DEFAULT = 'Pick a venue'
     
-    venue_list = match_df['venue'].unique()    
-    venue = st.selectbox('Select Venue',sorted(venue_list))
+    venue_list = utils.getVenueList(comb_df)
+    season_list = utils.getSeasonList(comb_df)
+    venue = utils.selectbox_with_default(st,'Select venue',venue_list,DEFAULT)
+    start_year, end_year = st.select_slider('Season',options=season_list, value=(2008, 2022))
     
+    if venue != DEFAULT:
     
-    if st.button('Show Stats'):
-        InningsWinDF = utils.getPerInningsWinCount(match_df,venue)        
-        VenueStatsDF = utils.getVenueStats(comb_df,venue)
-                
-        final_df = pd.merge(VenueStatsDF , InningsWinDF, on = 'venue')
-        final_df.drop(['venue'], axis=1, inplace=True) 
-        
-        # CSS to inject contained in a string
-        hide_dataframe_row_index = """
-                    <style>
-                    .row_heading.level0 {display:none}
-                    .blank {display:none}
-                    </style>
-                    """
+        filtered_df = utils.getSeasonDataFrame(comb_df,start_year,end_year)
+        if not filtered_df.empty:
+            InningsWinDF = utils.getPerInningsWinCount(match_df,venue)        
+            VenueStatsDF = utils.getVenueStats(comb_df,venue)
+                    
+            final_df = pd.merge(VenueStatsDF , InningsWinDF, on = 'venue')
+            final_df.drop(['venue'], axis=1, inplace=True) 
+            
+            # CSS to inject contained in a string
+            hide_dataframe_row_index = """
+                        <style>
+                        .row_heading.level0 {display:none}
+                        .blank {display:none}
+                        </style>
+                        """
 
-        # Inject CSS with Markdown
+            # Inject CSS with Markdown
+            
+            st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+            st.table(final_df.style.format(precision=2))
+            
+            comb_df = pd.merge(comb_df, player_df[['Player_Name','bowling_style']], left_on='bowler', right_on='Player_Name', how='left')
+            comb_df.drop(['Player_Name'], axis=1, inplace=True)
+            
+            bowlingstats_df = utils.getBowlingStatsforaVenue(comb_df,venue)        
+            st.subheader('Bowling Style Comparison')
+            
+            st.table(bowlingstats_df.style.format(precision=2))
+        else:
+                st.subheader('No Data Found!')
         
-        st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-        st.table(final_df.style.format(precision=2))
-        
-        comb_df = pd.merge(comb_df, player_df[['Player_Name','bowling_style']], left_on='bowler', right_on='Player_Name', how='left')
-        comb_df.drop(['Player_Name'], axis=1, inplace=True)
-        
-        bowlingstats_df = utils.getBowlingStatsforaVenue(comb_df,venue)        
-        st.subheader('Bowling Style Comparison')
-        
-        st.table(bowlingstats_df.style.format(precision=2))
-     
-    
