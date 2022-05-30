bfb4269820a0032775e2536300f461ce2a1b8e46
gaurav.j <gaurav.j@media.net>
2022-05-30 09:08:52 +0530

    added query param to distinguish diff page visits

--- multiapp.py
+++ multiapp.py
@@ -2,6 +2,13 @@
 """
 import streamlit as st
 
+def get_position(spages,option):
+    for i in range(len(spages)):
+        
+        if spages[i]['title'] == option:
+            break
+    return i
+    
 class MultiApp:
     """Framework for combining multiple streamlit applications.
     Usage:
@@ -23,6 +30,7 @@ class MultiApp:
     """
     def __init__(self):
         self.apps = []
+        self.index=0
 
     def add_app(self, title, func):
         """Adds a new application.
@@ -35,13 +43,46 @@ class MultiApp:
         """
         self.apps.append({
             "title": title,
-            "function": func
+            "function": func,            
         })
-
+        self.index = self.index + 1
+        
     def run(self):
+        tot_pages = len(self.apps)
+        cols = st.sidebar.columns(tot_pages)
+        query_params = st.experimental_get_query_params()
+        if query_params:
+            sel_index = get_position(self.apps,query_params['option'][0])
+        else:
+            sel_index =0
         app = st.sidebar.radio(
-            'Go To',
-            self.apps,
-            format_func=lambda app: app['title'])
+           'Go To',
+           self.apps,
+           index=sel_index,
+           format_func=lambda app: app['title'])
+        
+        but_values = ['false' for i in range(tot_pages)]
+                
+        try:
+            query_option = app['title']            
+        except:
+            st.experimental_set_query_params(option=app['title'])
+            query_params = st.experimental_get_query_params()
+            query_option = query_params['option'][0]
+        q_index = get_position(self.apps,query_option)
+        but_values[q_index] = True
+        if "cmenu" not in st.session_state:
+            st.session_state.cmenu=0
 
-        app['function']()
+        if True in but_values:
+            c_index = but_values.index(True)            
+            if q_index is c_index:                
+                final = c_index
+                st.experimental_set_query_params(option=self.apps[final]['title'])
+            else:                
+                final = q_index
+        st.session_state.cmenu=final
+        self.apps[st.session_state.cmenu]['function']()
+        return self.apps[st.session_state.cmenu]['title']
+
+        
